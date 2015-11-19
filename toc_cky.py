3af3a5751d8b17165dfb07145a9070cda259d56d
Kiran Kumar Lekkala <kiran@linuxcreatures.com>
2015-11-19 20:15:41 +0530

    input_process function removed

--- toc_cky.py
+++ toc_cky.py
@@ -8,39 +8,7 @@ input_string = input_file.read()
 a = input_string.split()
 
 
-input_list = [i.strip().split() for i in open("input_cyk.txt").readlines()]
 
-D = ['#']
-B = []
-S = ['#',1]
-Q = []
-Q.append(D)
-
-for i in range(0,len(input_list)):
-
-	B.append('#')
-	B = input_list[i][::2]
-	Q.append(B)
-	B = []
-
-R = [item[0] for item in Q]
-
-
-def find(a,b):
-	for i in range(1,len(R)):
-		if a == R[i]:
-			tup1 = i
-		if b == R[i]:
-			tup2 = i
-	return (tup1,tup2)
-	
-
-for j in range(1,len(Q)):
-	for k in range(1,len(Q[j])):
-		if len(Q[j][k])==2:
-			Q[j][k] = find(Q[j][k][0],Q[j][k][1])
-
-print Q
 #r is the number of non-terminal symbols
 r = len(Q)
 #P is a 3D dimetional array. Initialized to zeroes.
